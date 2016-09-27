885564b5760688f813ee72c7258c0d174504a02e
Daniel Miller <dm635@cornell.edu>
2016-09-26 21:25:00 -0400

    some new code

--- dds-ch2.py
+++ dds-ch2.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot
 
 # Use GGplot stype.
 matplotlib.style.use('ggplot')
+def plot():
+    matplotlib.pyplot.show()
 
 # Read NYT ad click data.
 data = pandas.read_csv("http://stat.columbia.edu/~rachel/datasets/nyt1.csv")
@@ -13,4 +15,17 @@ data = pandas.read_csv("http://stat.columbia.edu/~rachel/datasets/nyt1.csv")
 data["Age_group"] = pandas.cut(data["Age"], [-numpy.inf, 18, 24, 34, 44, 54, 64, numpy.inf])
 
 # Histograms for number of impressions, for each age group.
-number_impressions = data["Impressions"].hist(by=data["Age"])
+data.hist("Impressions", by="Age_group")
+plot()
+
+# Create new feature: click-through rate.
+data["Click_through"] = data["Clicks"] / data["Impressions"]
+
+# Plot distribution of click-through rate, for each age group.
+data.hist("Click_through", by="Age_group")
+plot()
+
+# Plot distribution of non-zero click-through rates (i.e., distribution of 
+#users who actually click), for each age group.
+data[data.Click_through != 0].hist("Click_through", by="Age_group")
+plot()
