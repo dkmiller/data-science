f5036c067f5e84bbcfbbfee1c31fc12f7d14853a
Daniel Miller <dm635@cornell.edu>
2017-04-24 11:30:45 -0400

    a bit more

--- cds-04.R
+++ cds-04.R
@@ -3,6 +3,11 @@ library(tidyverse)
 
 setwd("C:/Users/dm635/Downloads")
 
-cancer <- read_csv("cancer.csv")
+cancer <- read_csv("cancer.csv") %>% select(-X33)
 
 cancer_modified <- cancer %>% select(-id, -diagnosis)
+
+# no. 3 and 5 are closest (ids 84300903 & 84358402)
+dist(cancer_modified %>% head(5))
+
+hc_cancer <- hclust(dist(cancer_modified), "ward.D2") %>% as.dendrogram()
